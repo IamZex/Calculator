71d21af0b5a11c8e0f3c55763c5b6d116854725b
IamZex <dbzmm1@yahoo.com>
2022-02-20 13:00:42 -0600

    updating to ask for help

--- calculator.js
+++ calculator.js
@@ -1,18 +1,63 @@
 var displayValue ='0',
     storedValue ='0',
     returnedValue='0';
+    operandValue =""
 
 const numberPress = document.querySelectorAll('.number').forEach(number => number.addEventListener('click', numPress));
 const operatorPress = document.querySelectorAll('.function').forEach(operator =>operator.addEventListener('click',funPress));
 const output = document.querySelector('.output');
 
 function funPress(event){
+    displayValue = parseFloat(displayValue)
     if (event.srcElement.id === 'C'){
     displayValue ='0'
+
 }else if (event.srcElement.id === 'CE'){
     displayValue ='0'
     storedValue ='0'
     returnedValue ='0'
+
+}else if (event.srcElement.id === '↢'){
+    displayValue = displayValue.toString();
+    displayValue.slice(0,(displayValue.length -1 ));
+    console.log('new' +displayValue);
+    displayValue = parseFloat(displayValue);
+
+}else if (event.srcElement.id === '1/X'){
+    displayValue =(1 / displayValue);
+
+}else if (event.srcElement.id === 'square'){
+    displayValue = displayValue* displayValue; 
+
+}else if (event.srcElement.id === '√'){
+    displayValue = Math.sqrt(displayValue);
+
+}else if (event.srcElement.id === '%'){
+    displayValue = 100 / displayValue;
+
+}else if (event.srcElement.id === '÷'){
+    operandValue = '÷';
+    storedValue = displayValue;
+    displayValue = '0';
+
+}else if (event.srcElement.id === 'x'){
+    operandValue = 'x';
+    storedValue = displayValue;
+    displayValue = '0';
+
+}else if (event.srcElement.id === '-'){
+    operandValue = '-';
+    storedValue = displayValue;
+    displayValue = '0';
+
+}else if (event.srcElement.id === '+'){
+    operandValue = '+';
+    storedValue = displayValue;
+    displayValue = '0';
+
+}else if (event.srcElement.id === '='){
+    console.log(operandValue)
+    displayValue = operate()
 }
     output.innerHTML = displayValue
 };
@@ -25,7 +70,6 @@ function numPress(event){
        displayValue = -displayValue 
     }else{
     displayValue += event.srcElement.id
-    console.log('click')
     }output.innerHTML = displayValue
 };
 function add(a,b){
@@ -42,32 +86,28 @@ function multiply(a,b){
 function divide(a,b){
     if (b ===0) {
         return "The universe disapproves"
-    }
+    }lelse
     return a / b
-}
-
-function percent(num){
-    return num * .01
-}
-function clearEverything(){
-// this should clear the display and all values
-}
-function clear(){
-//this should clear just the previous value
-}
-function backspace(){
-//this will back up one character until 0
-}
-function oneOver(a){
-    return 1/a
-}
-function squared(a){
-    return Math.pow(a,2);
-}
-function  squareRoot(a){
-    return Math.sqrt(a);
+    
 }
 
 function operate(o,a,b){
+    o = operandValue
+    a = parseFloat(storedValue)
+    b = parseFloat(displayValue)
 
-}
+    if (o = '+'){ 
+        return  add(a,b);
+
+    }else if(o = '-'){
+
+        return  subtract(a,b);
+    }else if(o = '÷'){
+        
+        return  divide(a,b);
+
+    }else if(o = 'x'){
+      return multiply(a,b);
+    } 
+
+};
